4ff1b65f0542a3641e5ee4f7ad43efa7d6b74e1b
Insopitus <jessefor@hotmail.com>
2020-03-04 00:16:58 +0800

    修改代码结构

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# the-solar-system
+# The Solar System
  trying to build the solar system w/ three.js
 
 It's just a rough model of the sun and the 8 planets atm.
 I may add satelites to it in the future.
+
 All the planets' data is from [Wikipedia](https://en.wikipedia.org/wiki/Solar_System)
+
+## UPDATE on March 3th, 2020
+I just found out that if I use the real ratios of the planets and the distances between each other, these planets would be so small that you couldn't even spot them, which made the sun so "lonely". So I decide to make fake ratios just to make the model look better.
+
+## How to build
+I used parcel-bundler to pack this project. 
+If you want build it youself, just run `npm install`, then `npm run build`
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,88 @@
+import * as planets from './planets.json'
 import * as Three from 'three'
+import { Vector3 } from 'three'
 const scene = new Three.Scene()
-const camera = new Three.PerspectiveCamera(75,window.innerWidth/window.innerHeight,.1,1000)
+const camera = new Three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
 const renderer = new Three.WebGLRenderer()
+const lightSource = new Three.SpotLight(0xffffff)
+lightSource.position.set(0, 0, 0)
+lightSource.castShadow = true
+scene.add(lightSource)
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 5
+console.log(planets)
+camera.lookAt(0, 0, 0)
 
-class Star{
-  radius:Number
-  color:String  
-  constructor(radius,color){
-    this.radius = radius
-  }
-}
-
-class Planet{
-  radius:Number //半径km
-  color:String
-  perihelion:Number //近日点km
-  aphelion:Number //远日点km
-  synodicPeriod:Number //会合周期day
-  obitalPeriod:Number //公转周期day
-  constructor(radius,color,perihelion,aphelion,synodicPeriod,obitalPeriod){
-    this.radius = radius
+class Star {
+  name: string
+  radius: number
+  color: string
+  constructor(name, radius, color) {
+    this.name = name
+    this.radius = radius / 1000
     this.color = color
-    this.perihelion = perihelion
-    this.aphelion = aphelion
-    this.synodicPeriod = synodicPeriod
-    this.obitalPeriod = obitalPeriod
+  }
+  draw() {
+    const geometry = new Three.SphereGeometry(this.radius / 10000, 32, 32)
+    const material = new Three.MeshBasicMaterial({ color: this.color })
+    const sphere = new Three.Mesh(geometry, material)
+    sphere.name = this.name
+    scene.add(sphere)
   }
 }
 
-const Sun = new Star(6.957e8,'red')
-const Mercury = new Planet(2438.7,'grey',4.6e10,6.98e10,115.88,87.9691)
-const Venus = new Planet(6051.8,'white',107.5e6,108.9e9,583.92,224.701)
-const Earth = new Planet(6371,'blue',147.095e6,152.1e6,1,365.26)
-const Mars = new Planet(3389.5,'orange',206.7e6,249.2e6,779.96,686.971)
-const Jupiter = new Planet(69911,'rgb(146,135,116)',740.52e6,916.62e6,398.88,4332.59)
-const Saturn = new Planet(58232,'yellow',1352.55e6,1514.50e6,378.09,10759.22)
-const Uranus = new Planet(25362,'whiteblue',2742e6,3008e6,369.66,30688.5)
-const Neptune = new Planet(24622,'lightblue',4.46e9,4.54e9,367.49,60182)
+class Planet {
+  name: string
+  radius: number //半径km
+  color: string
+  perihelion: number //近日点km
+  aphelion: number //远日点km
+  synodicPeriod: number //会合周期day
+  obitalPeriod: number //公转周期day
+  constructor(option) {
+    this.name = option.name
+    this.radius = option.radius / 1000
+    this.color = option.color
+    this.perihelion = option.perihelion / 1000
+    this.aphelion = option.aphelion / 1000
+    this.synodicPeriod = option.synodicPeriod / 1000
+    this.obitalPeriod = option.obitalPeriod / 1000
+  }
+  draw() {
+    // 修改行星参数使行星相比太阳大到可见
+    let ratio = 10
+    const geometry = new Three.SphereGeometry(this.radius * ratio, 26, 26)
+    const material = new Three.MeshBasicMaterial({ color: this.color })
+    const sphere = new Three.Mesh(geometry, material)
+    sphere.position.set((this.perihelion + this.aphelion) / 2, 0, 0)
+    sphere.name = this.name
+    console.log(sphere)
+    scene.add(sphere)
+  }
+  revolve() {}
+}
+
+const Sun = new Star('Sun', 695.7e3, 'red')
+const Mercury = new Planet(planets.fake.Mercury)
+const Venus = new Planet(planets.fake.Venus)
+const Earth = new Planet(planets.fake.Earth)
+const Mars = new Planet(planets.fake.Mars)
+const Jupiter = new Planet(planets.fake.Jupiter)
+const Saturn = new Planet(planets.fake.Saturn)
+const Uranus = new Planet(planets.fake.Uranus)
+const Neptune = new Planet(planets.fake.Neptune)
+
+function render() {
+  Sun.draw()
+  Earth.draw()
+  renderer.render(scene, camera)
+}
+render()
